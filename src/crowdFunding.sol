fd7174fc1cff67aa1de5ef7cf96464e99cfb1c14
Bright Bassey <basseyu01@gmail.com>
2025-06-15 20:53:56 +0100

    Initial commit

---
+++ src/crowdFunding.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract CrowdFunding {
+    //State
+    address public projectCreator;
+    uint public goal;
+    uint public deadline;
+    uint public amountRaised;
+    bool public goalReached;
+    bool public creatorWithdrawn;
+
+    mapping(address => uint) public contributions;
+
+    //modifiers
+
+    modifier onlyCreator() {
+        require(msg.sender == projectCreator, "Only creator can perform this function");
+        _;
+    }
+
+    modifier beforeDeadline() {
+        require(block.timestamp < deadline, "Deadline passed");
+        _;
+    }
+
+    modifier afterDeadline() {
+        require(block.timestamp >= deadline, "Deadline not reached yet");
+        _;
+    }
+    // Contructors
+    constructor(uint _goal, uint _durationInDays) {
+        projectCreator = msg.sender;
+        goal = _goal;
+        deadline = block.timestamp + (_durationInDays * 1 days);
+    }
+    //Functions
+
+    function contributeFunds() external payable beforeDeadline {
+        require(msg.value > 0, "Invalid Amount");
+
+        contributions[msg.sender] += msg.value;
+        amountRaised += msg.value;
+
+        if (amountRaised >= goal) {
+            goalReached = true;
+        }
+    }
+
+    function withdrawFunds() external onlyCreator {
+        require(goalReached == true, "Goal not met");
+        require(!creatorWithdrawn, "Funds already withdrawn");
+
+        creatorWithdrawn = true;
+        payable(projectCreator).transfer(address(this).balance);
+    }
+
+    function refund() external afterDeadline {
+        require(!goalReached, "Goal was reached");
+
+        uint amountContributed = contributions[msg.sender];
+        require(amountContributed > 0, "invalid request, No contribution to refund");
+
+        contributions[msg.sender] = 0;
+        payable(msg.sender).transfer(amountContributed);
+    }
+
+    function getTotalAmountContributed(address contributor) external view onlyCreator returns(uint){
+            return contributions[contributor];
+    }
+}
---
+++ test/crowdfunding.t.sol
@@ -0,0 +1,71 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {CrowdFunding} from "../src/crowdFunding.sol";
+
+contract testCrowdFunding is Test {
+    CrowdFunding public funding;
+    address contributor1;
+    address contributor2;
+    address creator;
+
+    function setUp() public {
+        contributor1 = address(0x1);
+        contributor2 = address(0x2);
+        creator      = address(this);
+        
+        funding = new CrowdFunding(2 ether, 10);
+
+        //
+        vm.deal(contributor1, 5 ether);
+        vm.deal(contributor2, 5 ether);
+    }
+
+    function testContributeUpdatesBalance() public {
+        vm.prank(contributor1);
+        funding.contributeFunds{value: 2 ether}();
+
+        uint balance = funding.contributions(contributor1);
+        assertEq(balance, 2 ether);
+    }
+    function testContributionsAndWithdraw() public {
+    vm.prank(creator);
+    funding = new CrowdFunding(2 ether, 10); // 10 days
+
+    // Fund accounts
+    vm.deal(contributor1, 5 ether);
+    vm.deal(contributor2, 5 ether);
+
+    
+    vm.prank(contributor1);
+    funding.contributeFunds{value: 3 ether}();
+
+    vm.prank(contributor2);
+    funding.contributeFunds{value: 3 ether}();
+
+    assertEq(funding.amountRaised(), 6 ether);
+    assertTrue(funding.goalReached());
+
+    
+    uint balanceBefore = creator.balance;
+    vm.prank(creator);
+    funding.withdrawFunds();
+    uint balanceAfter = creator.balance;
+
+    assertGt(balanceAfter, balanceBefore);
+    assertEq(address(funding).balance, 0);
+}
+
+    function testCannotRefundifDeadlineNotReached() public {
+        vm.prank(contributor1);
+        funding.contributeFunds{value: 2 ether}();
+
+        vm.warp(block.timestamp + 6 days);
+
+        vm.prank(contributor1);
+        vm.expectRevert("Deadline not reached yet");
+
+        funding.refund();
+    }
+}
